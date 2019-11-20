6fbddc2aa26d73ac4d6bc5f0bf194d98a2f5b9ce
sbs2001 <shivam.sandbhor@gmail.com>
2019-11-20 22:25:11 +0530

    new files

--- main.js
+++ main.js
@@ -39,4 +39,35 @@ else
 		email = document.getElementById('signupEmail').value;
 		password = document.getElementById('signupPass').value;
 		Users.push(new User(name,email,password));
-    
+		localStorage.setItem("Users", JSON.stringify(Users));
+		document.getElementById('signupName').value ='';
+		document.getElementById('signupEmail').value='';
+		document.getElementById('signupPass').value='';
+	});
+
+	document.getElementById('loginApply').addEventListener('click',function()
+	{
+		email = document.getElementById('loginMail').value;
+		password = document.getElementById('loginPass').value;
+		var found  = 0 ;
+		for(var i = 0 ; i < Users.length ; i++)
+		{
+			if(Users[i].email == email && Users[i].password == password)
+			{
+				document.getElementById('user').innerHTML = Users[i].name;
+				document.getElementById('greet').innerHTML = "Welcome! " + Users[i].name;
+				document.getElementById("signupform").style.display="none";
+				document.getElementById("LoginForm").style.display="none";
+				document.getElementById("forNewUser").style.display="none";
+				found =1;
+				break;
+			}
+
+		}
+		if(found==0)
+		{
+			window.alert("Enter a valid Username and correct Password");
+		}
+
+	});
+
